f665371aaab4657c8b5d4b799700006146ceb01f
squishpesolis <stalin.ct.ss@gmail.com>
2023-06-10 06:59:09 -0500

    add funtion to get many dataframes

---
+++ src/application/usecases/utils/tranform_file_to_get_many_dataframe_usecase.py
@@ -0,0 +1,28 @@
+from tokenize import String
+import typing
+from fastapi import UploadFile
+import pandas as pd
+from io import BytesIO
+
+from src.application.usecases.interfaces import GenericUseCase
+from src.application.utils.error_handling_utils import ErrorHandlingUtils
+from src.domain.dog_fact import DogFactEntity
+
+
+class TransformFileToGetManyDataFrameUseCase(GenericUseCase):
+    def __init__(self,fileUpload: UploadFile, sheets_name) -> None:
+        self.fileUpload = fileUpload
+        self.sheets_name = sheets_name
+
+    async def execute(self) -> pd.DataFrame:
+        try:
+
+         
+            contents = self.fileUpload.file.read()
+            data = BytesIO(contents)
+            df = pd.read_excel(data,sheet_name=self.sheets_name)
+            data.close()
+            return df
+        except Exception as exception:
+            print(exception)
+            raise ErrorHandlingUtils.application_error("No se puede obtener el dataframe del archivo excel cargado", exception)
--- src/application/usecases/views_ismart/create_views_main_usecase.py
+++ src/application/usecases/views_ismart/create_views_main_usecase.py
@@ -6,7 +6,9 @@ from src.application.usecases.interfaces import GenericUseCase
 from src.application.utils.error_handling_utils import ErrorHandlingUtils
 
 from src.application.usecases.utils.tranform_file_to_dataframe_usecase import TransformFileToDataFrameUseCase
+from src.application.usecases.utils.tranform_file_to_get_many_dataframe_usecase import TransformFileToGetManyDataFrameUseCase
 from src.application.usecases.views_ismart.create_view_admin_dashboard_usecase import CreateViewAdminDashboardUseCase
+
 
 from src.application.usecases.enums.names_sheet_excel_ismart_configuration_enum import SheetsNameExcelConfigISmart;
 
@@ -20,10 +22,16 @@ class CreateViewMainUseCase(GenericUseCase):
     async def execute(self):
         try:
             df_views =  pd.DataFrame()
-            tranform_file_to_dataframe_usecase:TransformFileToDataFrameUseCase =  TransformFileToDataFrameUseCase(self.file, SheetsNameExcelConfigISmart.AreasSK.value)
-            dataframe_areas = await tranform_file_to_dataframe_usecase.execute()
+            
+            #tranform_file_to_dataframe_usecase:TransformFileToDataFrameUseCase =  TransformFileToDataFrameUseCase(self.file, SheetsNameExcelConfigISmart.AreasSK.value)
+            #dataframe_areas = await tranform_file_to_dataframe_usecase.execute()
 
-            create_view_admin:CreateViewAdminDashboardUseCase = CreateViewAdminDashboardUseCase(dataframe_areas,self.configurar_con_entidades_demos) 
+            tranform_file_to_dataframe_usecase:TransformFileToGetManyDataFrameUseCase =  TransformFileToGetManyDataFrameUseCase(self.file, [SheetsNameExcelConfigISmart.AreasSK.value, SheetsNameExcelConfigISmart.Entidades.value])
+            dataframes = await tranform_file_to_dataframe_usecase.execute()
+
+
+
+            create_view_admin:CreateViewAdminDashboardUseCase = CreateViewAdminDashboardUseCase(dataframes.get(SheetsNameExcelConfigISmart.AreasSK.value),self.configurar_con_entidades_demos) 
             await create_view_admin.execute()
             print("")
         except Exception as exception:
